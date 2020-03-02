b519fb402cb1c2e015c116fcc9b38cf721cf0a1d
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-02 12:21:50 -0800

    onClick functionality for boxes
    can add  'X' to clicked boxes.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,7 +4,19 @@ console.log('we are here');
 
 //click handler for boxes (add X/ add O, etc.)
 
+//identify all buttons
+var buttons = document.getElementsByClassName("button");
 
-var buttons = document.getElementsByClassName('button');
+//NOTE: I played around with this forever knowing the logic and code was correct
+//but with zero results. This is because I put the script section in the html above
+//the place where the button html elements are generated. PAY ATTENTION in future...
 
-console.log(buttons);
+for (const button of buttons) {
+//iterates through buttons and signs event listener to each
+console.log(button);
+  button.addEventListener('click', function(event) {
+    console.log('click!');
+    button.innerHTML = 'X';
+  });
+
+};
