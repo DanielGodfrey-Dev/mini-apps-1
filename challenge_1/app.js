e3d10d242bf1dc5d4f8f7a389c81524db19fa39c
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-02 14:21:05 -0800

    Establish board, ID's, matrix, and test for placement

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -11,30 +11,44 @@ var buttons = document.getElementsByClassName("button");
 //but with zero results. This is because I put the script section in the html above
 //the place where the button html elements are generated. PAY ATTENTION in future...
 
+//GAME
+//generate board
+
+
 for (const button of buttons) {
 //iterates through buttons and signs event listener to each
 console.log(button);
   button.addEventListener('click', function(event) {
     console.log('click!');
     button.innerHTML = 'X';
+ 	var position = button.id;
+ 	if (position > 0 && position <= 3) {
+ 		board[0][position - 1] = 'X';
+ 	} else if (position > 3 && position <= 6) {
+ 		board[1][position - 4] = 'X';
+ 	} else if (position > 6 && position <= 9) {
+ 		board[2][position - 7] = 'X';
+ 	}
   });
 
 };
 
-//GAME
-//generate board
 var board = [
 			[null, null, null],
 			[null, null, null],
 			[null, null, null],
 			];
 
-			console.log(board);
-
 //once a box is checked, cannot change
 
 //while there are moves left and no win..
+
+while (gameover = false) {
+var gameover = false;
+var player1 = true;
 //Player one marks an 'X'
+
+
 	//check for win
 //Player two marks an 'O'
 	//check for win
@@ -45,4 +59,6 @@ var board = [
 	//win? lose?
 //Player 2 Box - potential O's remaining
 	//win? lose?
+
+}
 
