a4ef4317b28e5f45be3bd3deea1d48eaa26cca84
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-03 10:52:16 -0800

    Establish package.json and initialize server
    Server listening successfully on port 3000.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,8 @@
+const express = require('express')
+const app =  express()
+const port = 3000
 
+app.get('/', (req, res) => res.send('we are online'))
+
+app.listen(port, () => console.log(`...I hear you on port ${port}...`))
 
