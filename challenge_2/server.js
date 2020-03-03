0ebd42c58e870d6ef38359ef9666daafa3fec5f0
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-03 11:42:09 -0800

    Added basic CSS to derail insanity from looking at a boring page all day...

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app =  express()
 const port = 3000
+const path = require('path');
 
 app.get('/', (req, res) => res.send('we are online'))
 
 app.listen(port, () => console.log(`...I hear you on port ${port}...`))
+
+app.use('/', express.static(path.join(__dirname, '../client')))
 
