e0fafd168c7c5ef6617e71d73780bebc89c02284
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-04 15:00:46 -0800

    Eliminate import statements in clien app.jsx
    probably an issue with not using webpack and transpiling from client to public folder...no need to import react library? can be used when referenced with HTML script tags and react.js files in public.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 
 console.log('we listened. we learned.');
 console.log('why');
 console.log('thrice');
 console.log('no comment.')
+
+ReactDOM.render(
+  <h4>Hello, world!</h4>,
+  document.getElementById('root')
+);
