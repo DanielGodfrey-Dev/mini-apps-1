f28b55c2731db4389d8ed546d8f3b74baed7089e
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-04 11:25:18 -0800

    Serving up index.html
    +style.css & app.js (currently empty)

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.send('we hear you'))
+
+app.listen(port, () => console.log(`we listen. port ${port}`))
 
 
