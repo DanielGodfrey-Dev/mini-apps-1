d8df40ea577b7039913a65b0050c61366056f343
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-04 14:46:40 -0800

    Add react files to public dir instead of doing script tags

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 
 
 console.log('we listened. we learned.');
-console.log('we build.');
-console.log('we test');
-console.log('what can be done');
-var x = 'yes';
-console.log('again');
-console.log('one more time');
+console.log('why');
+console.log('thrice');
+console.log('no comment.')
