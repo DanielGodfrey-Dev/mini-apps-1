c6df3930609106e6da3a2bc2aeb75c48dfea1caf
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-04 12:33:05 -0800

    Transpiling correctly working on HTML script

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 
+console.log('we listened. we learned.');
+console.log('we build.');
+console.log('we test');
+console.log('what can be done');
+var x = 'yes';
+console.log('again');
+console.log('one more time');
