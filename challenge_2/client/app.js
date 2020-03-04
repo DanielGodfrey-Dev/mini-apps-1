8a87b2a210e91e0dc598e1e8214aa9b49566b3f1
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-03 17:37:59 -0800

    Psuedocode --- blocked debugging
    cannot get express POST function to ding with HTML form. Outlined solution in psuedocode. Will implement later with time.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,10 @@
+//!!!---need to psuedocode only for lack of time in order to implement
+//later or revisit in the future---!!!
 
+//this is going to be some JQuery code to render the csv_report
+//style data to the DOM.
+
+//since the main function appears to be specified to live
+//only on server.js, for now limited functionality here should
+//be fine.
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,12 +1,24 @@
+//!!!---need to psuedocode only for lack of time in order to implement
+//later or revisit in the future---!!!
+
 const express = require('express')
 const app =  express()
 const port = 3000
 const path = require('path');
 
-app.get('/', (req, res) => res.send('we are online'))
-app.post('/upload', (req, res) => res.send('we got something'))
-
-app.listen(port, () => console.log(`...I hear you on port ${port}...`))
-
+//define root directory
 app.use('/', express.static(path.join(__dirname, '../client')))
 
+//for handling form submissions
+app.use(express.json())
+
+
+app.get('/', (req, res) => res.send('we are online')) // working
+app.post('/upload', (req, res) => { //why not working?!?
+
+  console.log('we are listening');
+  //need to parse data and format to csv, return and render to DOM
+
+});
+
+app.listen(port, () => console.log(`...I hear you on port ${port}...`))
