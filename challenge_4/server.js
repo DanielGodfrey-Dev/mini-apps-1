392bcae180803c0f8e8aba00e58907354e5a030a
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-05 10:20:59 -0800

    Express initialization successful.

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('we listen. we acknowledge.'));
+
+app.listen(port, () => console.log(`gyrosyncopating on port ${port}`));
+
 
 
